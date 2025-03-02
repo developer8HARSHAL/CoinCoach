650ea5fe7979d53bf6d6289d7cbcd9c1b6e4a996
Bhavana <bhavanapillai.100@gmail.com>
2025-03-02 13:44:29 +0530

    Updated drag and drop folder structure

--- src/app/components/Quiz.js
+++ src/app/components/Quiz.js
@@ -5,7 +5,7 @@
 // import Link from 'next/link';
 // import Drag_Drop from './Drag_Drop';
 
-import Drag_Drop from "./Drag_Drop";
+import Drag_Drop from "./DragAndDrop/Drag_Drop";
 
 // const QuizComponent = () => {
 //   // const questions = [
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Homepage from "./components/Homepage";
 import CoinCoachLearning from "./components/CoinCoachLearning";
-import InvestmentBasics from "./inverstment/investment-basics";
+import InvestmentBasics from "./components/LearningModulesComponents/investment-basics";
 
 export default function Home() {
   return (
