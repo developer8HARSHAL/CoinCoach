c00eb2068c7c74f997b220c1cb30ded995ca7f0d
Yashada7 <yashadatembe07@gmail.com>
2025-03-02 20:40:58 +0530

    Credit & Debt Management page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,8 +1,8 @@
 'use client';
 
 import React from 'react';
-import Dashboard from '../components/Dashboard/Dashboard';
-import { UserDataProvider } from '../components/Dashboard/UserDataProvider';
+import Dashboard from '../components/Dashboard';
+import { UserDataProvider } from '../components/Dashboard';
 import Dashboard from '../components/dashboard/dashboard';
 import { UserDataProvider } from '../components/dashboard/UserDataProvider';
 
